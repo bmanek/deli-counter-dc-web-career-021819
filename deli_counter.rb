63c293080e61a6dc25fb57119f3539b17ad88523
Ben Manekin <ben.manekin@gmail.com>
2019-01-31 00:10:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    array.each_with_index { |x, index| "#{array[index + 1]}. #{x}" << 
+    "The line is currently: "}
+  end
+end
+
+
+def take_a_number(array, string)
+  array.push string
+  
+end
+
+
+def now_serving
+end
+
+
