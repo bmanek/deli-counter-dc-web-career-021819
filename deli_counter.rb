87e16a31f84e78c31972d1e62d71ab3d57eee905
Ben Manekin <ben.manekin@gmail.com>
2019-01-31 00:18:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    array.each_with_index { |x, index| "#{array[index + 1]}. #{x}" << 
-    "The line is currently: "}
+    array.each_with_index { |x, index| "The line is currently: " << "#{array[index + 1]}. #{x}" 
+    }
   end
 end
 
